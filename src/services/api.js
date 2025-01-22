79176fe870750a2ef605ba64ba664874d822e560
Xandr <ravnb1989@gmail.com>
2025-01-22 16:02:11 +0200

    api connected

--- src/services/api.js
+++ src/services/api.js
@@ -1,22 +1,26 @@
 import axios from "axios";
 
-const ACCESS_KEY = 'r-M2Ot83Y4mEP5HC2gXpz-OD-rW4ZoCXG-sfFLZTGlM';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const ACCES_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZmI5NTM0OWIxYTQ1ZjEzM2FmNGQyZDA3YjI3Y2JjZSIsIm5iZiI6MTczNzU1MTQ2Mi41MzksInN1YiI6IjY3OTBlZTY2YzkxMTVjOWUzZDI4YjRiMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.w5cYJoSeoBDfKHT2ocAG9ZjFxNpG1nULJsIOtoynddY';
 
-axios.defaults.baseURL = 'https://api.unsplash.com';
-axios.defaults.headers = {
-    Authorization: `Client-ID ${ACCESS_KEY}`,
-}
-axios.defaults.params = {
-    per_page: 15,
-    orientation: 'landscape',
-}
-
-export const fetchData = async (query, page) => {
-    const { data } = await axios.get(`/search/photos`, {
-        params: {
-            query,
-            page,
-        }
-    });
-    return data;
+const options = {
+    headers: {
+        Authorization: `Bearer ${ACCES_TOKEN}`,
+    },
 };
+
+
+export const fetchData = async () => {
+    try {
+        const { data } = await axios.get(`${BASE_URL}/trending/movie/day`, options);
+        console.log(data);
+        return data;
+        
+        
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+
